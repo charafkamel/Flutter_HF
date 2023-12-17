798475a55eaeba0ad313bb9506d70161b96d08bc
charafkamel <charafkamel@live.com>
2023-12-17 12:38:06 +0100

    deleted

--- flutter_homework_start_23_2/android/app/src/main/kotlin/com/example/flutter_homework_start_23_2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_homework_start_23_2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter_homework_start_23_2/lib/ui/bloc/list/list_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'list_bloc.dart';
-
-@immutable
-abstract class ListEvent extends Equatable {}
-
-class ListLoadEvent extends ListEvent {
-  @override
-  List<Object?> get props => [];
-}
